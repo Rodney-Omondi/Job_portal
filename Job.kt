431aed5df24d26cda56bad1f640f7fbfcb8acdf4
Rodney-Omondi <rodney.ngaa@strathmore.edu>
2025-12-21 07:45:12 -0800

    Add files via upload

---
+++ Job.kt
@@ -0,0 +1,9 @@
+package com.example.jobportal.model
+
+data class Job(
+    val id: String,
+    val title: String,
+    val company: String,
+    val location: String,
+    val description: String
+)
