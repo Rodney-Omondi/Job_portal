a192f6e428c37f04395a744a7e76d25e28defdab
Rodney-Omondi <rodney.ngaa@strathmore.edu>
2025-12-24 01:18:53 -0800

    Delete Job.kt

--- Job.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jobportal.model
-
-data class Job(
-    val id: String,
-    val title: String,
-    val company: String,
-    val location: String,
-    val description: String
-)
